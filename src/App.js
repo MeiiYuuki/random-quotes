a6f67eccc46f7b07032b8c2a5f1836567a7bb1be
unknown <velicia.wong@ho.ipeka.sch.id>
2024-03-25 15:27:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    quote: "",
+    backgroundUrl: "",
+  };
+
+  componentDidMount() {
+    this.fetchQuote();
+    this.fetchBackgroundImage();
+  }
+
+  fetchQuote = async () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        this.setState({
+          quote: response?.data?.slip?.advice,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  fetchBackgroundImage = async () => {
+    axios
+      .get(
+        "https://api.unsplash.com/photos/random/?topics=nature&content_filter=low&orientation=landscape&client_id=eECFAGsCNO4ty-Gq1L3Ing_CoLwN2pn0PvIDjbEtr6k"
+      )
+      .then((response) => {
+        this.setState({
+          backgroundUrl: response.data.urls.full,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div
+        className="container"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${this.state.backgroundUrl})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      >
+        <div className="card">
+          <h1 className="heading">{this.state.quote}</h1>
+          <button className="button" onClick={this.fetchQuote}>
+            <span>I need a quote !</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
